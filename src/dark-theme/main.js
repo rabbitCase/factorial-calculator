4800a6b38f346ec8bdf867f4b10cd21598837478
rabbitCase <gnair3004@gmail.com>
2025-03-01 23:20:44 +0530

    included handling for no integral inputs

--- src/dark-theme/main.js
+++ src/dark-theme/main.js
@@ -1,29 +1,31 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    function fact(i) {
-        if (i <= 1) {
-            return 1;
-        }
-        return i * fact(i - 1);
+function fact(i) {
+    if (i <= 1) {
+        return 1;
     }
-    const textBoxValueSetter = (value) => document.getElementById("fact-text").value = value;
-    const button = document.querySelector("#fact-button");
-    button.addEventListener("click", () => {
-        const x = document.getElementById("fact-text").value;
-        if(!x){
-            textBoxValueSetter("Please enter a number");
-            return;
-        }
-        
-        else if(x<0){
-            textBoxValueSetter("Undefined!");
-            return;
-        }
+    return i * fact(i - 1);
+}
+let prompt= "Please enter a number";
+const textBoxValueSetter = (value) => document.getElementById("fact-text").value = value;
+const button = document.querySelector("#fact-button");
+button.addEventListener("click", () => {
+    const x = document.getElementById("fact-text").value;
+    const num=Number(x);
+    if(!x){
+        textBoxValueSetter(prompt);
+        return;
+    }
 
-        else{
-            textBoxValueSetter(null);
-        }
-        
-        const result= fact(x);
-        textBoxValueSetter(`Factorial of ${x} is ${result}.`);
-    });
+    else if(isNaN(num)){
+        textBoxValueSetter(null);
+        return;
+    }
+    
+    else if(num < 0 || num % 1 !== 0){
+        textBoxValueSetter("Undefined!");
+        return;
+    }
+
+    const result= fact(num);
+    textBoxValueSetter(`Factorial of ${x} is ${result}.`);
 });
+
